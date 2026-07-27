1facb6da256d543f3cbe02de59fec0619bdad331
Yogeshbodake18 <Yogesh.bodake@flintinfotech.com>
2026-07-27 12:29:25 +0530

    change the sidebar

--- src/config/adminNav.ts
+++ src/config/adminNav.ts
@@ -75,7 +75,7 @@ export const adminNavItems: AdminNavEntry[] = [
     ],
   },
     {
-    label: "Academics Modules",
+    label: "Academics Module",
     icon: MdAssignmentTurnedIn,
     children: [
        { label: "Subject Assignment", path: "/subject-assignment", icon: MdAssignment },
