7a749567aca1811bc6ef166fc6ab9435325219ad
vikasaldar4 <Vikas.aldar@flintinfotech.com>
2026-09-01 18:22:23 +0530

    account modul added

--- src/config/adminNav.ts
+++ src/config/adminNav.ts
@@ -95,13 +95,21 @@ export const adminNavItems: AdminNavEntry[] = [
         {label:"Results", path:"/results",icon:PiClipboardText},
         {label:"Achievements",path:"/achievements",icon:RiMedalLine },
         {label:"Academic calendar", path:"/academic-calender", icon:SlCalender},
-        {label:"Student Fees", path:"/student-fees",icon:BsCash},
         {label:"Time Table", path:"/time-table",icon:PiCalendarBlank}
     ],
   },
   {label: "Profile",path: "/profile",icon: ImProfile,hideInSidebar: true,},
   {
     label:"Student Profile", path:"/student-profile",icon:PiStudentFill
+  },
+   {
+    label: "Accounts",
+    icon: BsCash,
+    children: [
+    
+        {label:"Student Fees", path:"/student-fees",icon:BsCash},
+      
+    ],
   },
   
  
